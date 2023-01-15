5c7fb7d95e111102707de49ffe15c4a903afc828
DanyMarinho <cardosodanielly11@gmail.com>
2023-01-15 17:11:29 -0300

    Modificações

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 var textInput = document.querySelector("#input-texto");
 var outInput = document.querySelector("#output");
 
+function criptografar(){
+    var texto = textInput.value;
 
-function criptografar() {
-    var texto = textInput.Value;
-
-    var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat")
-
-
-    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultCripto +
-    '<textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+  var result1 = texto.replace(/e/g, "enter");
+  var result2 = result1.replace(/i/g, "imes");
+  var result3 = result2.replace(/a/g, "ai");
+  var result4 = result3.replace(/o/g, "ober");
+  var result5 = result4.replace(/u/g, "ufat");
+  document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + result5 + 
+  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
-function descriptografar() {
-    var texto = textInput.Value;
-
-    var resultCripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "ober").replace(/ufat/g, "u")
-
-
-    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescripto +
-    '<textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+function descriptografar(){
+    var texto = textInput.value;
+  
+    var result1 = texto.replace(/enter/g, "e");
+    var result2 = result1.replace(/imes/g, "i");
+    var result3 = result2.replace(/ai/g, "a");
+    var result4 = result3.replace(/ober/g, "o");
+    var result5 = result4.replace(/ufat/g, "u");
+  
+    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + result5 + 
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
 function copiar() {
     var textoCop = document.getElementById('input-texto');
-
+  
     textoCop.select();
     document.execCommand('copy');
-    alert("Texto copiado.");
-}
+    alert("Texto copiado para área de transferência.");
+}  
